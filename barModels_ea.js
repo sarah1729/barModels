45bc36e6620d3bcff7066fb47bf472cec5a514d6
Eric Appleton <ecappleton@gmail.com>
2022-01-06 21:31:18 -0500

    added Notes document with a bit of plan

--- barModels_ea.js
+++ barModels_ea.js
@@ -1,19 +1,26 @@
 //declaration of variables
-var draw;
-
+var c;
 
 //main action
+/* init(); */
 
+//init function to initialize the game
+/* function init(){
+    drawCanvas();
+} */
 
 
 //draw canvas for bar model
 function drawCanvas(){
-    draw = document.getElementById("barModelCanvas");
-    var ctx = draw.getContext("2d");
+    c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
     ctx.beginPath();
-    ctx.rect(40,20,100,20);
+    ctx.rect(40,20,300,20);
     ctx.stroke();
-    
+    ctx.fillStyle = "#FF2022";
+    ctx.fillRect(40,20,80,20);
+    console.log('drawCanvas function');
+
 }
 
-drawBarModelButton.addEventListener('click', drawCanvas);
+/* drawRect.addEventListener('click', drawCanvas); */
